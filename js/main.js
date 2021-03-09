89a7175fdec3dcf8b5f92ef505dbb5eb9ac59a27
Ritik Aggarwal <55681241+ritikagg2907@users.noreply.github.com>
2021-03-09 22:52:36 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            560: {
+                items: 2
+            }
+        }
+    })
+
+    let nav_offset_top = $('.header_area').height() + 50;
+
+    function navbarFixed() {
+        if ($('.header_area').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+});
